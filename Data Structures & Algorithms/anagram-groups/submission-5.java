343f80263556b43952f2cfba562b833a576bd651
Alexandr Radulov <alex.radulov03@gmail.com>
2026-06-23 21:43:55 +0300

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        // idea is count the letters then
+        // after receiving the count group them according to how
+        // many letters they have
+
+        Map<Map<Character, Integer>, List<String>> amountsToWords 
+            = new HashMap<>(); 
+
+        for(String str : strs) {
+        
+            Map<Character, Integer> letterAmount = new HashMap<>();
+
+            for(char c : str.toCharArray()) { 
+                Integer count = letterAmount.putIfAbsent(c,1);
+                if(count != null) {
+                    letterAmount.put(c, count + 1);
+                }
+            }
+
+            amountsToWords.computeIfAbsent(
+                letterAmount, 
+                k -> new ArrayList<>())
+            .add(str);
+
+        }
+
+        return amountsToWords.values().stream().toList();
+
+    }
+}
